4f7d69a45776439ab70dd5f5873c8a07e1d11c5c
lwalker <landen@example.com>
2023-08-02 10:48:43 -0500

    date data

--- 02-Challenge/javascript.js
+++ 02-Challenge/javascript.js
@@ -9,24 +9,24 @@ document.getElementById('search').addEventListener('click', function() {
     .then(function(data){
         console.log(data)
         var currentDay = data.list[0];
-        document.getElementById('temp').innerText = 'temperature: ' + currentDay.main.temp + ' degrees Fahrenheit';
-        document.getElementById('wind').innerText = 'wind: ' + currentDay.wind.speed + ' MPH';
-        document.getElementById('humidity').innerText = 'humidity: ' + currentDay.main.humidity + '%';
+        document.getElementById('date').innerText = 'Date: ' + currentDay.dt_txt;
+        document.getElementById('temp').innerText = 'Temperature: ' + currentDay.main.temp + ' degrees Fahrenheit';
+        document.getElementById('wind').innerText = 'Wind: ' + currentDay.wind.speed + ' MPH';
+        document.getElementById('humidity').innerText = 'Humidity: ' + currentDay.main.humidity + '%';
 
         var j = 0
         for (var i = 7; i < data.list.length; i+=8) {
             var day = data.list[i];
             console.log(day)
             j++;
+            document.getElementById('date' + j).innerText = 'Date: ' + day.dt_txt;
             document.getElementById('temp' + j).innerText = 'temperature: ' + day.main.temp + ' degrees Fahrenheit';
             document.getElementById('wind' + j).innerText = 'wind: ' + day.wind.speed + ' MPH';
             document.getElementById('humidity' + j).innerText = 'humidity: ' + day.main.humidity + '%';
             
+            console.log(day);
 
-            // document.getElementById('temp2').innerText = 'temperature: ' + day.main.temp + ' degrees Fahrenheit';
-            // document.getElementById('wind2').innerText = 'wind: ' + day.wind.speed + ' MPH';
-            // document.getElementById('humidity2').innerText = 'humidity: ' + day.main.humidity + '%';
-            console.log(day)
+      
 
             
         }
